81c0d93f20790a725a641f612db5860ef484f4ae
alicoder123411 <sameersingla240@gmail.com>
2026-01-19 17:05:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function minDate(dates) {
-	let min=dates[0]
-	for(let i=1;i<dates.length;i++){
-		if(min>dates[i]){
-			min=dates[i]
-		}
-	}
-	retrun min //write you code here
+  let min = dates[0];
+
+  for (let i = 1; i < dates.length; i++) {
+    if (dates[i] < min) {
+      min = dates[i];
+    }
+  }
+
+  alert(min);
 }
+
 
 // Do not change the code
 
